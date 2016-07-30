9ba252f577098f3a3deb15b8e62995c0a70f5b5b
Mario Geiger <geiger.mario@gmail.com>
2016-07-31 00:28:42 +0200

    details

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ extern crate bson;
 extern crate mongodb;
 
 extern crate rustc_serialize;
-extern crate crypto;
 extern crate rand;
 extern crate time;
 extern crate num_cpus;
@@ -24,8 +23,6 @@ use rustc_serialize::json;
 use rustc_serialize::json::Json;
 use bson::{Document, Bson};
 use std::io::Read;
-use crypto::md5::Md5;
-use crypto::digest::Digest;
 use rustc_serialize::hex::ToHex;
 use std::collections::btree_map::BTreeMap;
 use rand::Rng;
@@ -110,45 +107,38 @@ fn main() {
 			Ok(x) => x,
 			Err(e) => {
 				println!("create: {}", e);
-				return Ok(Response::with(status::BadRequest));
+				return Ok(Response::with((status::BadRequest, format!(r#"{{"error": "request error : {}"}}"#, e))));
 			}
 		};
 
 		if data.vmax.iter().fold(0, |acc, &x| acc + x) < (data.mails.len() as i32) {
 			println!("create: not enough room");
-			return Ok(Response::with(status::BadRequest));
+			return Ok(Response::with((status::BadRequest, r#"{"error": "more mails than slots"}"#)));
 		}
 		
 		if data.vmax.len() != data.vmin.len() || data.vmax.len() != data.slots.len() {
 			println!("create: array size problem");
-			return Ok(Response::with(status::BadRequest));
+			return Ok(Response::with((status::BadRequest, r#"{"error": "vmin, vmax and slots must have the same size"}"#)));
 		}
 		
 		if data.vmin.iter().zip(data.vmax.iter()).any(|(&xmin, &xmax)| xmin > xmax) {
 			println!("create: vmin > vmax");
-			return Ok(Response::with(status::BadRequest));			
+			return Ok(Response::with((status::BadRequest, r#"{"error": "there are vmin bigger than vmax"}"#)));			
 		}
 
-		let mut hasher = Md5::new();
-		let mut keys = Vec::new();
-		for mail in data.mails.iter() {
+		let mut keys = Vec::with_capacity(data.mails.len());
+		for _ in 0..data.mails.len() {
 			loop {
-				hasher.input(mail.as_bytes());
-				hasher.input(data.name.as_bytes());
-				let mut bytes: [u8; 4] = [0; 4];
-				rand::thread_rng().fill_bytes(&mut bytes);
-				hasher.input(&bytes);
-				let mut output: [u8; 16] = [0; 16];
-				hasher.result(&mut output);
-				hasher.reset();
-				let k = output.to_hex();
-				if keys.contains(&k) {
+				let mut key: [u8; 16] = [0; 16];
+				rand::thread_rng().fill_bytes(&mut key);
+				let key = key.to_hex();
+				if keys.contains(&key) {
 					continue;
 				}
-				if db.collection("events").find_one(Some(doc!{"people.key" => (k.clone())}), None).unwrap_or(None).is_some() {
+				if db.collection("events").find_one(Some(doc!{"people.key" => (key.clone())}), None).unwrap_or(None).is_some() {
 					continue;
 				}
-				keys.push(k);
+				keys.push(key);
 				break;
 			}
 		}
@@ -157,19 +147,19 @@ fn main() {
 		doc.insert_bson("name".to_owned(), Bson::String(data.name.clone()));
 		doc.insert_bson("deadline".to_owned(), Bson::I64(data.deadline));
 		doc.insert_bson("slots".to_owned(), Bson::Array(data.slots.iter().map(|s| Bson::String(s.clone())).collect()));
-		doc.insert_bson("vmin".to_owned(), Bson::Array(data.vmin.iter().map(|v| Bson::I32(*v)).collect()));
-		doc.insert_bson("vmax".to_owned(), Bson::Array(data.vmax.iter().map(|v| Bson::I32(*v)).collect()));
+		doc.insert_bson("vmin".to_owned(), Bson::Array(data.vmin.iter().map(|&v| Bson::I32(v)).collect()));
+		doc.insert_bson("vmax".to_owned(), Bson::Array(data.vmax.iter().map(|&v| Bson::I32(v)).collect()));
 		doc.insert_bson("people".to_owned(), {
-			let mut x = Vec::new();
+			let mut x = Vec::with_capacity(data.mails.len());
 			for i in 0..data.mails.len() {
 				let mut p = Document::new();
-				p.insert("mail", data.mails[i].clone());
-				p.insert("key", keys[i].clone());
+				p.insert_bson("mail".to_owned(), Bson::String(data.mails[i].clone()));
+				p.insert_bson("key".to_owned(), Bson::String(keys[i].clone()));
 				
-				p.insert("wish", {
+				p.insert_bson("wish".to_owned(), {
 					let mut v = Vec::new();
 					v.resize(data.slots.len(), Bson::I32(0));
-					v
+					Bson::Array(v)
 				});
 				x.push(Bson::Document(p));
 			}
@@ -178,13 +168,13 @@ fn main() {
 		
 		if let Err(e) = db.collection("events").insert_one(doc, None) {
 			println!("create: {}", e);
-			return Ok(Response::with((status::Ok, r#"{"error": "database"}"#)));
+			return Ok(Response::with((status::NotFound, format!(r#"{{"error": "database error : {}"}}"#, e))));
 		}
 		
 		let json = Json::Object({
 			let mut bt = BTreeMap::new();
 			bt.insert("people".to_owned(), Json::Array({
-				let mut v = Vec::new();
+				let mut v = Vec::with_capacity(data.mails.len());
 				for i in 0..data.mails.len() {
 					let mut obj = BTreeMap::new();
 					obj.insert("mail".to_owned(), Json::String(data.mails[i].clone()));
@@ -199,7 +189,7 @@ fn main() {
 			Ok(x) => x,
 			Err(e) => {
 				println!("create: {}", e);
-				return Ok(Response::with((status::Ok, r#"{"error": "unknown"}"#)));
+				return Ok(Response::with((status::NotFound, format!(r#"{{"error": "json error : {}"}}"#, e))));
 			}
 		};
 	
@@ -214,7 +204,7 @@ fn main() {
 			Ok(x) => x,
 			Err(e) => {
 				println!("fill: {}", e);
-				return Ok(Response::with((status::BadRequest, r#"{"error": "error when parse data"}"#)));
+				return Ok(Response::with((status::BadRequest, format!(r#"{{"error": "request error : {}"}}"#, e))));
 			}
 		};
 		
@@ -232,7 +222,7 @@ fn main() {
 			Ok(_) => {},
 			Err(e) => {
 				println!("info: {}", e);
-				return Ok(Response::with((status::NotFound, r#"{"error": "error with database"}"#)));
+				return Ok(Response::with((status::NotFound, format!(r#"{{"error": "database error : {}"}}"#, e))));
 			}
 		};
 		
@@ -261,39 +251,47 @@ fn main() {
 			Ok(x) => x,
 			Err(e) => {
 				println!("info: {}", e);
-				return Ok(Response::with((status::NotFound, r#"{"error": "database"}"#)));
+				return Ok(Response::with((status::NotFound, format!(r#"{{"error": "database error : {}"}}"#, e))));
 			}
 		};
 		
-		
-		if let Some(event) = event {
-			let people = event.get_array("people").unwrap();
-			let mut person = None;
-			for p in people.iter() {
-				if let &Bson::Document(ref p) = p {
-					if let Ok(k) = p.get_str("key") {
-						if k == &(data.key) {
-							person = Some(p);
+		match event {
+			None => Ok(Response::with((status::NotFound, r#"{"error": "database"}"#))),
+			Some(event) => {
+				let people = event.get_array("people").unwrap();
+				let mut person = None;
+				for p in people.iter() {
+					if let &Bson::Document(ref p) = p {
+						if let Ok(k) = p.get_str("key") {
+							if k == &(data.key) {
+								person = Some(p);
+							}
 						}
 					}
 				}
+				match person {
+					None => Ok(Response::with((status::NotFound, r#"{"error": "database"}"#))),
+					Some(person) => {
+						let json = json::encode(&OutputInfo { 
+							name: event.get_str("name").unwrap_or("").to_string(),
+							deadline: event.get_i64("deadline").unwrap_or(0),
+							mails: event.get_array("people").unwrap_or(&Vec::new()).iter().map(|p| match p {&Bson::Document(ref x) => x.get_str("mail").unwrap_or("@").to_owned(), _ => "@".to_owned()}).collect(),
+							mail: person.get_str("mail").unwrap_or("@").to_owned(),
+							wish: person.get_array("wish").unwrap_or(&Vec::new()).iter().map(|x| match x {&Bson::I32(v) => v, _ => 0}).collect(),
+							slots: event.get_array("slots").unwrap_or(&Vec::new()).iter().map(|x| match x {&Bson::String(ref v) => v.clone(), _ => "".to_owned()}).collect(),
+							results: event.get_array("results").unwrap_or(&Vec::new()).iter().map(|x| match x {&Bson::I32(v) => v, _ => 0}).collect()
+						});
+						let payload = match json {
+							Ok(x) => x,
+							Err(e) => {
+								println!("info: {}", e);
+								return Ok(Response::with((status::NotFound, format!(r#"{{"error": "json error : {}"}}"#, e))));
+							}
+						};
+						Ok(Response::with((status::Ok, payload)))
+					}
+				}
 			}
-			if let Some(person) = person {
-				let payload = json::encode(&OutputInfo{ 
-					name: event.get_str("name").unwrap_or("").to_string(),
-					deadline: event.get_i64("deadline").unwrap_or(0),
-					mails: event.get_array("people").unwrap_or(&Vec::new()).iter().map(|p| match p {&Bson::Document(ref x) => x.get_str("mail").unwrap_or("@").to_owned(), _ => "@".to_owned()}).collect(),
-					mail: person.get_str("mail").unwrap_or("@").to_owned(),
-					wish: person.get_array("wish").unwrap_or(&Vec::new()).iter().map(|x| match x {&Bson::I32(v) => v, _ => 0}).collect(),
-					slots: event.get_array("slots").unwrap_or(&Vec::new()).iter().map(|x| match x {&Bson::String(ref v) => v.clone(), _ => "".to_owned()}).collect(),
-					results: event.get_array("results").unwrap_or(&Vec::new()).iter().map(|x| match x {&Bson::I32(v) => v, _ => 0}).collect()
-				}).unwrap();
-				Ok(Response::with((status::Ok, payload)))
-			} else {
-				Ok(Response::with((status::NotFound, r#"{"error": "database"}"#)))
-			}
-		} else {
-			Ok(Response::with((status::NotFound, r#"{"error": "database"}"#)))
 		}
 	}
 	
@@ -313,20 +311,26 @@ fn main() {
 	
 		let event = {
 			let db = db.clone();
-			let db = db.lock().unwrap();
+			let db = match db.lock() {
+				Ok(x) => x,
+				Err(_) => return
+			};
 			db.collection("events").find_one(Some(query), None)
 		};
 
 		if let Ok(Some(event)) = event {			
-			let vmin = event.get_array("vmin").unwrap().iter().map(|x| match x { &Bson::I32(v) => v as u32, _ => 0 }).collect();
-			let vmax = event.get_array("vmax").unwrap().iter().map(|x| match x { &Bson::I32(v) => v as u32, _ => 0 }).collect();
-			let wishes = event.get_array("people").unwrap().iter().map(|p| match p { &Bson::Document(ref p) => p.get_array("wish").unwrap().iter().map(|x| match x {&Bson::I32(v) => v as u32, _ => 0}).collect(), _ => Vec::new()} ).collect();
+			let vmin = event.get_array("vmin").unwrap_or(&Vec::new()).iter().map(|x| match x { &Bson::I32(v) => v as u32, _ => 0 }).collect();
+			let vmax = event.get_array("vmax").unwrap_or(&Vec::new()).iter().map(|x| match x { &Bson::I32(v) => v as u32, _ => 0 }).collect();
+			let wishes = event.get_array("people").unwrap_or(&Vec::new()).iter().map(|p| match p { &Bson::Document(ref p) => p.get_array("wish").unwrap_or(&Vec::new()).iter().map(|x| match x {&Bson::I32(v) => v as u32, _ => 0}).collect(), _ => Vec::new()} ).collect();
 			
 			let results = solver::search_solution(&vmin, &vmax, &wishes, 20f64);
 			if results.is_empty() { return; }
 			
-			let results = Bson::Array(results[0].iter().map(|x| Bson::I32(*x as i32)).collect());
-			db.lock().unwrap().collection("events").update_one(doc!{"_id" => (event.get_object_id("_id").unwrap().clone())}, doc!{"$set" => {"results" => results}}, None).unwrap();
+			let results = Bson::Array(results[0].iter().map(|&x| Bson::I32(x as i32)).collect());
+			match db.lock() {
+				Ok(x) => x,
+				Err(_) => return
+			}.collection("events").update_one(doc!{"_id" => (event.get_object_id("_id").unwrap().clone())}, doc!{"$set" => {"results" => results}}, None).unwrap();
 		}
 	}
 
