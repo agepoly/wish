01c9d524d1e3ccb0cd6f9854d8c351d8b99a6b21
louismerlin <louis.merlin@epfl.ch>
2016-10-05 15:45:56 +0200

    Add docker integration (big commit...)

---
+++ nginx/static/get.js
@@ -0,0 +1,98 @@
+//var key = window.location.pathname.split("/")[2];
+var key = window.location.hash.substring(1);
+var x = null;
+var deadline = null;
+var now = new Date();
+
+$(document).ready(function() {
+	console.log("hello world");
+
+	$.post("http://localhost:3000/info", '{ "key" : "'+key+'" }', function(data,status) {
+		if (status == "success") {
+			console.log("hi");
+			x = eval('(' + data + ')');
+			$("#name").text('Activity name: '+x.name);
+			$("#mail").text('Your email: '+x.mail);
+
+			deadline = new Date(x.deadline);
+
+			if (deadline > now) {
+				var hours = Math.floor((deadline-now)/1000/3600);
+				var days = Math.floor(hours / 24);
+				hours = hours % 24;
+				$("#deadline").text('Deadline: '+deadline.toJSON().split('T')[0]+' (in '+days+' days and '+hours+' hours)');
+			} else {
+				var days = Math.floor((now-deadline)/1000/3600/24);
+				$("#deadline").text('Deadline: '+deadline.toJSON().split('T')[0]+' ('+days+' days ago)');
+			}
+
+			if (x.results.length > 0) {
+				var content = '<table border="1"><tr><th>Slot Name</th><th>Mail</th></tr>';
+				var n = x.slots.length;
+				for (i=0; i< n; ++i) {
+					var list = [];
+					for (j=0; j < x.mails.length; ++j) {
+						if (x.results[j] == i) {
+							list.push(x.mails[j]);
+						}
+					}
+					content += '<tr><th>'+x.slots[i]+'</th><th>'+list.join(", ")+'</th></tr>';
+				}
+				content += '</table>';
+				$("#content").html(content);
+			} else {
+				var content = '<table border="1"><tr><th>Slot Name</th><th>Wish</th></tr>';
+				var n = x.slots.length;
+				for (i=0; i< n; ++i) {
+					content += '<tr><th>'+x.slots[i]+'</th><th><input type="number" name="wish'+i+'" min="1" max="'+n+'" step="1" value="'+(n-x.wish[i])+'"></th></tr>';
+				}
+				content += '</table>';
+				$("#content").html(content);
+				$("input").bind('input propertychange', upload_wish);
+				$("input").bind('input propertychange', color_wish);
+				color_wish();
+			}
+		} else {
+			console.log("hello");
+		}
+	});
+});
+
+function upload_wish() {
+	var wish = [];
+	var n = x.slots.length;
+	for (var i = 0; i < n; ++i) {
+		wish.push(n - Number($("input[name='wish"+i+"']").val()));
+	}
+
+	var data = '{ "key" : "'+key+'", "wish" : ['+wish.join(",")+'], "admin_key" : "" }';
+	console.log(data);
+
+	$.ajax({
+		type: "POST",
+		url: "http://localhost:3000/fill",
+		data: data,
+		success: function(data) {
+			console.log(data);
+			$("input").css({'border-color' : ''});
+			$("#error").text('');
+		},
+		error: function(data) {
+			console.log(data);
+			$("input").css({'border-color' : '#FF0000'});
+			$("#error").text('The values are not valid: each number must appear one time');
+		},
+	});
+}
+
+function color_wish() {
+	var colormap = ['#40ff00', '#80ff00', '#bfff00', '#ffff00', '#ffbf00', '#ff8000', '#ff4000', '#ff0000'];
+	var n = x.slots.length;
+	for (var i = 0; i < n; ++i) {
+		var v = n - Number($("input[name='wish"+i+"']").val());
+		// v = 0 .. n-1
+		v = v * (colormap.length - 1) / (n - 1);
+		v = Math.round(v);
+		$("input[name='wish"+i+"']").css({'background-color' : colormap[v]});
+	}
+}
---
+++ nginx/static/home.js
@@ -0,0 +1,130 @@
+
+$(document).ready(function() {
+	$("input[name='nslots']").bind('input propertychange', create_slots);
+	$("#send").click(send);
+	$("input").bind('input propertychange', check_validity);
+	$("textarea").bind('input propertychange', check_validity);
+	$("input[name='deadline']").prop('min', new Date().toJSON().split('T')[0]);
+	create_slots();
+	check_validity();
+});
+
+function send() {
+	var slots = get_slot_val();
+	var deadline = new Date($("input[name='deadline']").val()).getTime() / 1000;
+	
+	var payload = '{'
+		+'"name"     : "'+$("input[name='name']").val()+'", '
+		+'"deadline" : '+deadline+', '
+		+'"mails"    : ["'+$("#mails").val().split(/[\s,]+/).join('","')+'"], '
+		+'"slots"    : ["'+slots.slot.join('","')+'"], '
+		+'"vmin"     : ['+ slots.vmin.join(',')  +'], '
+		+'"vmax"     : ['+ slots.vmax.join(',')  +']'
+		+'}';
+	
+	console.log(payload);
+	
+	$.post("http://localhost:3000/create", payload, function(data,status) {
+		if (status == "success") {
+			var x = eval('(' + data + ')');
+			var url = window.location.hostname + ":" + window.location.port + "/get";
+			var content = "";
+			for (i=0; i < x.people.length; ++i) {
+				content += x.people[i].mail+" "+url+"#"+x.people[i].key+"\n";
+			}
+			$("#output").html(content);
+			$("#admin_url").html(url+"#"+x.admin_key);
+		}
+	});
+}
+	
+function create_slots() {
+	var n = $("input[name='nslots']").val();
+	var oldvalues = get_slot_val();
+	
+	var content = "<table><tr><th>Slot Name</th><th>Min Bound</th><th>Max Bound</th></tr>";
+	for (i=0; i < n; ++i) {
+		var values = {
+			name: "no "+(i+1),
+			vmin: "0",
+			vmax: "10"
+		};
+		if (i < oldvalues.slot.length) {
+			values.name = oldvalues.slot[i];
+			values.vmin = oldvalues.vmin[i];
+			values.vmax = oldvalues.vmax[i];
+		}
+		content += '<tr><th><input type="text" class="slot" name="slot'+i+'" value="'+values.name+'"></th>'
+			+ '<th><input type="number" class="vmin" name="vmin'+i+'" min="0" max="100" step="1" value="'+values.vmin+'"></th>'
+			+ '<th><input type="number" class="vmax" name="vmax'+i+'" min="0" max="100" step="1" value="'+values.vmax+'"></th></tr>'
+	}
+	content += "</table>";
+	$("#slots").html(content);
+	$("input").bind('input propertychange', check_validity);
+}
+
+function get_slot_val() {
+	var slot = [];
+	var vmin = [];
+	var vmax = [];
+	for (var i = 0; $("input[name='slot"+i+"']").length; ++i) {
+		slot.push($("input[name='slot"+i+"']").val());
+		vmin.push($("input[name='vmin"+i+"']").val());
+		vmax.push($("input[name='vmax"+i+"']").val());
+	}
+	
+	return { slot: slot, vmin: vmin, vmax: vmax };
+}
+
+function check_validity() {
+	$("input").removeAttr('style');
+	$("#mails").removeAttr('style');
+
+	if ($("input[name='name']").val() == "") {
+		$("input[name='name']").css({'border-color' : '#FF0000'});
+	}
+	
+	var max_capacity = 0;
+	
+	for (var i = 0; $("input[name='slot"+i+"']").length; ++i) {
+		if ($("input[name='slot"+i+"']").val() == "") {
+			$("input[name='slot"+i+"']").css({'border-color' : '#FF0000'});
+		}
+		var vmin = Number($("input[name='vmin"+i+"']").val());
+		var vmax = Number($("input[name='vmax"+i+"']").val());
+		if (vmin > vmax) {
+			$("input[name='vmin"+i+"']").css({'border-color' : '#FF0000'});
+			$("input[name='vmax"+i+"']").css({'border-color' : '#FF0000'});
+		}
+		max_capacity += vmax;
+	}
+	
+	if ($("#mails").val() == "") {
+		$("#mails").css({'border-color' : '#FF0000'});
+	}
+
+	var mails = $("#mails").val().split(/[\s,]+/);
+
+	for (var i = 0; i < mails.length; ++i) {
+		if (mails[i] == "") {
+			$("#mails").css({'border-color' : '#FF0000'});
+		}
+	}
+	
+	if (mails.length > max_capacity) {
+		$(".vmax").css({'border-color' : '#FF0000'});
+		$("#mails").css({'border-color' : '#FF0000'});
+	}
+	
+	mails.sort();
+	for (var i = 1; i < mails.length; ++i) {
+		if (mails[i-1] == mails[i]) {
+			$("#mails").css({'border-color' : '#FF0000'});
+		}
+	}
+	
+	
+	if ($("input[name='deadline']").val() == "") {
+		$("input[name='deadline']").css({'border-color' : '#FF0000'});
+	}
+}
