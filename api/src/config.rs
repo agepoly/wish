7ce6c223be4ab5c1edafb071dce50f20b05963aa
Mario Geiger <geiger.mario@gmail.com>
2016-12-26 20:57:39 +0100

    Delete config.rs

--- api/src/config.rs
+++
@@ -1,5 +0,0 @@
-pub static MAIL_SERVER : &'static str = "smtp1.epfl.ch";
-pub static MAIL_PORT : u16 = 25;
-pub static MAIL_USER : &'static str = "";
-pub static MAIL_PASSWORD : &'static str = "";
-
