49df639928d6aa37ca5c25cd3cb8ecc08772dfba
Mario Geiger <geiger.mario@gmail.com>
2017-02-13 23:43:55 +0100

    rm config.rs

--- src/config.rs
+++
@@ -1,4 +0,0 @@
-pub static MAIL_SERVER : &'static str = "mail.epfl.ch";
-pub static MAIL_PORT : u16 = 465;
-pub static MAIL_USER : &'static str = "mgeiger";
-pub static MAIL_PASSWORD : &'static str = "e45tgv.lop00";
