02ee2a19a1aaea58551b6ec00f275d61c6cb321c
Mario Geiger <geiger.mario@gmail.com>
2017-10-05 10:44:44 +0200

    offline.html CodeMirror theme

--- static/offline.js
+++ static/offline.js
@@ -21,16 +21,24 @@ function initDOM() {
     });
 
     INPUT_CODE = CodeMirror.fromTextArea(document.getElementById('input'), {
-        lineNumbers: true,
+        lineNumbers: false,
+        scrollbarStyle: "null",
+        viewportMargin: Infinity,
+        lineWrapping: true,
         mode: "csv",
         gutters: ["CodeMirror-lint-markers"],
-        lint: true
+        lint: true,
+        theme: 'wish'
     });
 
     OUTPUT_CODE = CodeMirror.fromTextArea(document.getElementById('output'), {
-        lineNumbers: true,
+        lineNumbers: false,
+        scrollbarStyle: "null",
+        viewportMargin: Infinity,
+        lineWrapping: true,
         mode: "csv",
-        readOnly: true
+        readOnly: true,
+        theme: 'wish'
     });
 
     var default_content = {
